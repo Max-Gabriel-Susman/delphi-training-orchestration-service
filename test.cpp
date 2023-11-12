d6c07c7d1a7c8cce0f35c67e84af935c1dcb908a
Gabe Susman <gabe.susman@gmail.com>
2023-11-11 22:14:43 -0700

    restored pretemplate implementation

---
+++ test.cpp
@@ -0,0 +1,30 @@
+// test.cpp
+#include <gtest/gtest.h>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+TEST(AddTest, PositiveNumbers) {
+    EXPECT_EQ(add(2, 3), 5);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+// test.cpp
+// #include <gtest/gtest.h>
+
+// int add(int a, int b) {
+//     return a + b;
+// }
+
+// TEST(AddTest, PositiveNumbers) {
+//     EXPECT_EQ(add(2, 3), 5);
+// }
+
+// int main(int argc, char** argv) {
+//     ::testing::InitGoogleTest(&argc, argv);
+//     return RUN_ALL_TESTS();
+// }
